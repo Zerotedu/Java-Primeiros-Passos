f99c9841d1c572727896553081732af2d63ee4ea
Zerotedu <132523972+Zerotedu@users.noreply.github.com>
2023-06-02 12:43:40 -0300

    Add files via upload

---
+++ TestaCaracteres.java
@@ -0,0 +1,25 @@
+
+public class TestaCaracteres {
+	
+	public static void main(String[] args) {
+		char letra = 'a'; // aspas simples e apenas UM caractere
+			System.out.println(letra);
+			
+			char valor = 66; // char recebe numeros como codigos, nao numeros. (ex: 66 = B)
+			
+			System.out.println(valor);
+			
+			valor = (char) (valor + 1);
+			
+			System.out.println(valor);
+			
+			String palavra = "alura cursos online de tecnologia";
+			
+			System.out.println(palavra);
+			
+			palavra = palavra + 2020;
+			
+			System.out.println(palavra);
+					
+	}
+}
